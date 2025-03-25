fbdf8b4bceff7fc757ef169bf7d574fa1f00726a
volt <kovalprog@yandex.ru>
2025-03-25 21:47:20 +0300

    Добавьте файлы проекта.

---
+++ src/bitwise.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef BITWISE_H
+#define BITWISE_H
+
+
+
+#endif // !BITWISE_H
---
+++ src/bools.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifndef BOOLS_H
+#define BOOLS_H
+#include "bitwise.h"
+
+#endif // !BOOLS_H
