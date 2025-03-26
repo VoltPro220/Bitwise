552ea10fff44afb1d6b8c4bef76e7574b6fe8d4b
volt <kovalprog@yandex.ru>
2025-03-26 17:16:38 +0300

    Add some defines

--- src/bitwise.h
+++ src/bitwise.h
@@ -3,6 +3,11 @@
 #ifndef BITWISE_H
 #define BITWISE_H
 
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitOn(value, bit) ((value) |= (1UL << (bit)))
+#define bitOff(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#define bit(bit) (1UL << (bit))
 
 
 #endif // !BITWISE_H
