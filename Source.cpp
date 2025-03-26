7906554634550f281a3e020d09f5f36c2783e716
volt <kovalprog@yandex.ru>
2025-03-26 21:40:59 +0300

    First mini version

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,7 @@
 #include "src/bitwise.h"
-#include <iostream>
-
 
 int main(void)
 {
-	short a = 10;
-	short b = 10;
-
-	std::cout << "Before\t" << a << std::endl;
-	devVal(a, 2);
-	std::cout << "after\t" << a << std::endl;
 
 	return 0;
 }
--- src/bitwise.h
+++ src/bitwise.h
@@ -2,15 +2,14 @@
 
 #ifndef BITWISE_H
 #define BITWISE_H
-#include <stdarg.h>
 
 #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
 #define bitSet(value, bit) ((value) |= (1UL << (bit)))
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
-#define bit(bit) (1UL << (bit))
 
 #define devVal(value, d) (value >>= (d-1))
-#define powVal(value, d) ((d == 0 ) ? (value = 1) : (d == 1) ? (value = value) : (value <<= (d-1)))
+#define powVal(value, d) ((d == 0) ? (value = 1) : (d == 1) ? (value = value) : (value <<= (d-1)))
 
+#define bit(bit) (1UL << (bit))
 #endif // !BITWISE_H
